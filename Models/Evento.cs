1c9178c531c4f5d2521b85ec6e2b1b22bdbf3417
GustavoAlonsoValleBustamante <102782638+GustavoAlonsoValleBustamante@users.noreply.github.com>
2024-09-21 16:48:05 -0500

    Update Evento.cs

--- Models/Evento.cs
+++ Models/Evento.cs
@@ -18,6 +18,7 @@ namespace proyecto_si8811a_2024_ii_u1_apis_y_funciones_jarro_y_valle.Models
 
         public string Facultad { get; set; }
         public string Resultado { get; set; }
+        public string Descripcion { get; set; }
 
     }
 }
