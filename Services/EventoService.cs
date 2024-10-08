0eea0b1df968cbbed35b042a835a48cbf86197ac
GustavoAlonsoValleBustamante <102782638+GustavoAlonsoValleBustamante@users.noreply.github.com>
2024-10-08 15:24:12 -0500

    Update EventoService.cs

--- Services/EventoService.cs
+++ Services/EventoService.cs
@@ -24,6 +24,12 @@ namespace proyecto_si8811a_2024_ii_u1_apis_y_funciones_jarro_y_valle.Services
         public virtual async Task<Evento> GetByIdAsync(string id) =>
             await _eventos.Find<Evento>(e => e.Id == id).FirstOrDefaultAsync();
 
+        public virtual async Task<List<Evento>> GetByNombreAsync(string nombre)
+        {
+            var filter = Builders<Evento>.Filter.Regex(e => e.Nombre, new MongoDB.Bson.BsonRegularExpression(nombre, "i"));
+            return await _eventos.Find(filter).ToListAsync();
+        }
+
         public virtual async Task<Evento> CreateAsync(Evento nuevoEvento)
         {
             await _eventos.InsertOneAsync(nuevoEvento);
